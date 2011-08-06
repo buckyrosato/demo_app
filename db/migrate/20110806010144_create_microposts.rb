7fac743f90ae64f0526887bfe8d933ddc77028fb
Bucky Rosato <b.t.rosato@gmail.com>
2011-08-05 18:16:48 -0700

    Demo App Completed

---
+++ db/migrate/20110806010144_create_microposts.rb
@@ -0,0 +1,15 @@
+class CreateMicroposts < ActiveRecord::Migration
+  def self.up
+    create_table :microposts do |t|
+      t.string :content
+      t.integer :user_id
+      t.string :tag
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :microposts
+  end
+end
